fb235a66fcd34fd9d18818dbf297705c0ce8a9b9
Ryan Dulac <analyticrye@gmail.com>
2019-01-12 08:43:49 -0500

    animation fadeout not working

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 var menu = document.getElementById('main-nav');
 var closeMenu = document.getElementById('close-menu');
+var openMenu = document.getElementById("open-menu");
 
 document.getElementById('open-menu').addEventListener('click',function () {
     menu.classList.add('show-nav');
+    menu.classList.remove('hide-nav')
+    openMenu.style.display = 'none';
+    closeMenu.style.display = "block";
+    
+    
 });
 document.getElementById('close-menu').addEventListener('click',function () {
-    menu.classList.remove('show-nav');
+    // menu.classList.remove('show-nav');
+    menu.classList.add('hide-nav');
+    // menu.classList.remove("show-nav");
+    openMenu.style.display = 'block';
+    closeMenu.style.display = "none";
+
 });
